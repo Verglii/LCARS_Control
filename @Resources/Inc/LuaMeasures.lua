133d99fe04a46f7ef472770c3fb12550da7ec9cd
Verglii <kedroviv@yandex.ru>
2013-11-17 22:37:16 +0600

    ver 0.58

--- @Resources/Inc/LuaMeasures.lua
+++ @Resources/Inc/LuaMeasures.lua
@@ -7,15 +7,3 @@ CPU = tonumber(SKIN:GetVariable('CPULCount'))
 	end
 end
 
--- function CpuNumber()
--- local ReadyN=2
-	-- CPU = SKIN:GetVariable('CPULCount')
-	-- while (ReadyN <= 12) do
-		-- if(ReadyN<CPU)then
-			-- SKIN:Bang('!SetVariable', ('CPN'+tostring(ReadyN)), 'ReadyN')
-		-- else 
-			-- SKIN:Bang('!SetVariable', ('CPN'+tostring(ReadyN)), '0')
-		-- end
-	-- ReadyN=ReadyN+1
-	-- end
--- end
---
+++ @Resources/Inc/Settings.lua
@@ -0,0 +1,22 @@
+function Control()
+Stage=tonumber(SKIN:GetVariable('Settings_Stage'))
+Cycle=0
+while Cycle <= 8 do
+	if Stage == Cycle then
+		SKIN:Bang('!SetVariable', 'SSt' .. tostring(Cycle), '0')
+		SKIN:Bang('!WriteKeyValue', 'Variables', 'SSt' .. tostring(Cycle), '0', '#@#Inc/Settings.inc')
+	else
+		SKIN:Bang('!SetVariable', 'SSt' .. tostring(Cycle), '1')
+		SKIN:Bang('!WriteKeyValue', 'Variables', 'SSt' .. tostring(Cycle), '1', '#@#Inc/Settings.inc')
+	end
+	Cycle=Cycle+1
+end
+end
+
+function SetValue()
+Var=None
+Value=None
+Place=None
+	SKIN:Bang('!SetVariable', tostring(Var), tostring(Value))
+	SKIN:Bang('!WriteKeyValue', 'Variables', tostring(Var), tostring(Value), tostring(Place))
+end
