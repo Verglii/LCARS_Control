d3673d6f4f7382d9e99a76d56f08644ee4804f0d
Verglii <kedroviv@yandex.ru>
2013-12-13 13:02:20 +0600

    ver 0.8 prerelease
    Almost ready 1280x768

--- @Resources/Inc/LuaMeasures.lua
+++ @Resources/Inc/LuaMeasures.lua
@@ -1,9 +1,20 @@
 function Initialize()
 ReadyN=2
+CPDis=1
 CPU = tonumber(SKIN:GetVariable('CPULCount'))
 	while ReadyN<=CPU do
 		SKIN:Bang('!SetVariable', 'CPN' .. tostring(ReadyN), ReadyN)
 		ReadyN=ReadyN+1
+	end
+	while CPDis<=12 do
+		if CPDis<=CPU then
+			SKIN:Bang('!SetVariable', 'CP' .. tostring(CPDis), 0)
+			SKIN:Bang('!WriteKeyValue', 'Variables', 'CP' .. tostring(CPDis), '0', '#@#Inc/Settings.inc')
+		else
+			SKIN:Bang('!SetVariable', 'CP' .. tostring(CPDis), 1)
+			SKIN:Bang('!WriteKeyValue', 'Variables', 'CP' .. tostring(CPDis), '1', '#@#Inc/Settings.inc')
+		end
+		CPDis=CPDis+1
 	end
 end
 
