81b1781fcfd4963c075ffb693a8f8051bbb05ca6
Verglii <kedroviv@yandex.ru>
2014-05-24 18:47:58 +0600

    8.4

--- @Resources/Inc/LuaMeasures.lua
+++ @Resources/Inc/LuaMeasures.lua
@@ -10,9 +10,13 @@ CPU = tonumber(SKIN:GetVariable('CPULCount'))
 		if CPDis<=CPU then
 			SKIN:Bang('!SetVariable', 'CP' .. tostring(CPDis), 0)
 			SKIN:Bang('!WriteKeyValue', 'Variables', 'CP' .. tostring(CPDis), '0', '#@#Inc/Settings.inc')
+			SKIN:Bang('!SetOption', 'CPU' .. tostring(CPDis) .. 'Value', 'Text', '#Core'..tostring(CPDis)..'N#  %1%')
+			SKIN:Bang('!SetOption', 'MeterCPUInd' .. tostring(CPDis), 'Y', '(560-[MeasureCPU'..tostring(CPDis)..']*2.3)')
 		else
 			SKIN:Bang('!SetVariable', 'CP' .. tostring(CPDis), 1)
 			SKIN:Bang('!WriteKeyValue', 'Variables', 'CP' .. tostring(CPDis), '1', '#@#Inc/Settings.inc')
+			SKIN:Bang('!SetOption', 'CPU' .. tostring(CPDis) .. 'Value', 'Text', ' ')
+			SKIN:Bang('!SetOption', 'MeterCPUInd' .. tostring(CPDis), 'Y', '560')
 		end
 		CPDis=CPDis+1
 	end
